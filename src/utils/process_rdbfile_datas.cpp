64fbbba822d9d85d4230f08da8f7ff7840ef890f
unknown <franckhebo@gmail.com>
2024-10-09 16:32:17 +0100

    keys with expiry

--- src/utils/process_rdbfile_datas.cpp
+++ src/utils/process_rdbfile_datas.cpp
@@ -126,9 +126,10 @@ std::pair<std::vector<std::any>, std::vector<std::any>> get_keys_values_from_fil
     std::ifstream input_file(filepath, std::ios::binary);
     if (std::filesystem::is_directory(filepath) || !input_file.is_open())
         return {};
-    std::vector<unsigned char> buffer((std::istreambuf_iterator<char>(input_file)),
+    std::vector<char> buffer0((std::istreambuf_iterator<char>(input_file)),
                               std::istreambuf_iterator<char>());
-    
+
+    std::vector<unsigned char> buffer(buffer0.begin(), buffer0.end());
     std::stringstream ss;
     std::vector<std::any> keys;
     std::vector<std::any> values;
