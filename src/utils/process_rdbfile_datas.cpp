07d683f3d1881beadef54b81faa261ac5c719395
unknown <franckhebo@gmail.com>
2024-10-09 14:39:10 +0100

    keys with expiry

--- src/utils/process_rdbfile_datas.cpp
+++ src/utils/process_rdbfile_datas.cpp
@@ -162,8 +162,8 @@ std::pair<std::vector<std::any>, std::vector<std::any>> get_keys_values_from_fil
        if ((int)(unsigned char)buffer[index] == 252) { 
             if (check_key_date_validity(buffer, index, eight_bytes)){
                 unsigned char binary_num[8];
-                for (int i = 8, j = 0; i > 0; i--, j++)
-                    binary_num[j] = buffer[index+i];
+                for (int j = 0; j < 8; j++)
+                    binary_num[j] = buffer[index+j+1];
                 auto now = std::chrono::steady_clock::now();
                 // Convert the current time to time since epoch
                 auto duration = now.time_since_epoch();
