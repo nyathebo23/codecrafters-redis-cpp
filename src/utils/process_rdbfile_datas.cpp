cece468d6e2e523247564a939075b83577ffe20c
unknown <franckhebo@gmail.com>
2024-10-09 18:52:16 +0100

    keys with expiry

--- src/utils/process_rdbfile_datas.cpp
+++ src/utils/process_rdbfile_datas.cpp
@@ -160,7 +160,6 @@ std::pair<std::vector<std::any>, std::vector<std::any>> get_keys_values_from_fil
             if (check_key_date_validity(buffer, index, eight_bytes)){
                 index += 9;
                 get_key_value_pair(buffer, index, keys, values);
-                return std::make_pair(keys, values);
             }
             else 
                 skip_key(buffer, index, eight_bytes);
