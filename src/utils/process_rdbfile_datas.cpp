12488a2d63a012bb9f597a6dd66a63ad2ae91bfa
unknown <franckhebo@gmail.com>
2024-10-09 16:10:12 +0100

    keys with expiry

--- src/utils/process_rdbfile_datas.cpp
+++ src/utils/process_rdbfile_datas.cpp
@@ -92,7 +92,7 @@ std::string decode_str_length(int& index, std::vector<unsigned char>& buffer){
 }
 
 void get_key_value_pair(std::vector<unsigned char>& buffer, int &index, std::vector<std::any>& keys, std::vector<std::any>& values){
-    if ((int)buffer[index] == 0){
+    if (buffer[index] == 0x00){
         index++;
         keys.push_back(decode_str_length(index, buffer));
         values.push_back(decode_str_length(index, buffer));
@@ -133,22 +133,22 @@ std::pair<std::vector<std::any>, std::vector<std::any>> get_keys_values_from_fil
     std::vector<std::any> values;
 
     int index = 0, buffer_size = buffer.size();
-    while (index < buffer_size && (int)buffer[index] != 254){
+    while (index < buffer_size && buffer[index] != 0xFE){
         index++;
     }
-    while (index < buffer_size && (int)buffer[index] != 251){
+    while (index < buffer_size && buffer[index] != 0xFB){
         index++;
     }
     index += 3;
-    while (index < buffer_size && (int)buffer[index] != 255){
-       if ((int)buffer[index] == 253) { 
+    while (index < buffer_size && buffer[index] != 0xFF){
+       if (buffer[index] == 0xFD) { 
             if (check_key_date_validity(buffer, index, four_bytes)){
                 index += 5;
                 get_key_value_pair(buffer, index, keys, values);
             }
             continue;     
        }
-       if ((int)buffer[index] == 252) { 
+       if (buffer[index] == 0xFC) { 
             if (check_key_date_validity(buffer, index, eight_bytes)){
                 index += 9;
                 get_key_value_pair(buffer, index, keys, values);
