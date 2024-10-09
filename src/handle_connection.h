afd7d66d47500d5c9a1ab58ba5bf0208c1f6a9d6
unknown <franckhebo@gmail.com>
2024-10-09 08:05:57 +0100

    get value key from rdb file

--- src/handle_connection.h
+++ src/handle_connection.h
@@ -1,6 +1,6 @@
 #ifndef HANDLE_CONNECTION_H
 #define HANDLE_CONNECTION_H
 
-void handle_connection(const int&, std::map<std::string, std::string>);
+void handle_connection(const int&, std::map<std::string, std::string>&);
 
 #endif
