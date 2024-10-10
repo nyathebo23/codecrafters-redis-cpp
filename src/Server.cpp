af52bc623a542a0052aa6748c77697e52ea95a1f
unknown <franckhebo@gmail.com>
2024-10-10 13:56:12 +0100

    keys with expiry

--- src/Server.cpp
+++ src/Server.cpp
@@ -25,6 +25,9 @@ int main(int argc, char **argv) {
       args_map[key2] = val2;
   }
 
+  int port = 6379;
+  if (argv[0] == '--port')
+      port = std::stoi(std::string(argv[1]));
   // Flush after every std::cout / std::cerr
   std::cout << std::unitbuf;
   std::cerr << std::unitbuf;
