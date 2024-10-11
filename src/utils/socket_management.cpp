4cc2fac01af58d4ad812d2b30b08ec0759ebe094
unknown <franckhebo@gmail.com>
2024-10-11 22:44:39 +0100

    process cmd on replicas

--- src/utils/socket_management.cpp
+++ src/utils/socket_management.cpp
@@ -315,6 +315,6 @@ int SocketManagement::send_handshake_to_master(int port){
     if (recv(server_fd, &buffer, sizeof(buffer), 0) <= 0) {
             std::cout << "azertyuiohggfgf";
     }    
-    retrieve_commands_from_master();
+    //retrieve_commands_from_master();
     return 1;
 }
