71049eefd9c54dc62455cc95560a17d5a9c1cbec
unknown <franckhebo@gmail.com>
2024-10-12 06:04:04 +0100

    receive ack cmd on replica and reply

--- src/utils/slave_socket_management.cpp
+++ src/utils/slave_socket_management.cpp
@@ -75,9 +75,10 @@ void SlaveSocketManagement::process_command(std::string data) {
 
 void SlaveSocketManagement::retrieve_commands_from_master(const int& serverfd) {
     while (1){
-        char buffer[128];    
-        if (recv(serverfd, &buffer, sizeof(buffer), 0) <= 0)
-            break;
+        char buffer[128]; 
+        recv(serverfd, &buffer, sizeof(buffer), 0);   
+        // if (recv(serverfd, &buffer, sizeof(buffer), 0) <= 0)
+        //     break;
         std::string data(buffer);
         // int pos = 0;
         // int end = data.find("*", 1);
