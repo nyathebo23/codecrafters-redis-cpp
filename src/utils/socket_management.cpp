ae30fff9d2ee3cb40feef2684984768becff9bea
unknown <franckhebo@gmail.com>
2024-10-14 15:02:57 +0100

    receive ack cmd on replica and reply

--- src/utils/socket_management.cpp
+++ src/utils/socket_management.cpp
@@ -31,34 +31,7 @@ void SocketManagement::handle_connection(int clientfd){
             break;
         }
         std::string data(buffer);
-        auto command_elts = this->get_command_array_from_rawdata(data);
-        std::string cmd = command_elts.first;
-        std::vector<std::string> extra_params = command_elts.second;
-
-        if (cmd == "echo"){
-            command_processing.echo(extra_params, clientfd);
-        }
-        else if (cmd == "ping"){
-            command_processing.ping(clientfd);
-        }
-        else if (cmd == "set"){
-            command_processing.set(extra_params, clientfd);
-        }
-        else if (cmd == "get"){
-            command_processing.get(extra_params, clientfd, extra_args["dir"] + "/" + extra_args["dbfilename"]);
-        }
-        else if (cmd == "config"){
-            command_processing.config(extra_params, clientfd, extra_args);
-        }
-        else if (cmd == "keys"){
-            command_processing.keys(extra_params, clientfd, extra_args["dir"] + "/" + extra_args["dbfilename"]);
-        }
-        else if (cmd == "info"){
-            command_processing.info(extra_params, clientfd, "slave");
-        }
-        else if (cmd == "replconf"){
-            command_processing.replconf(extra_params, clientfd);
-        }    
+        execute_command(data, clientfd);
     }
 }
 
@@ -92,7 +65,6 @@ SocketManagement::SocketManagement(short family, int type, std::map<std::string,
     int port = 6379;
     if (extra_args.count("port") != 0){
         port = std::stoi(extra_args["port"]);
-        std::cout << port;
     }
     server_addr.sin_port = htons(port);
 }
