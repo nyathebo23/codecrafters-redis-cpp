99075c1e7981df02c431fa69d366035b37fcbe21
unknown <franckhebo@gmail.com>
2024-10-14 13:31:28 +0100

    receive ack cmd on replica and reply

--- src/utils/socket_management.cpp
+++ src/utils/socket_management.cpp
@@ -20,7 +20,41 @@
 #include "command_processing.h"
 
 void SocketManagement::execute_command(std::string buffer_data, int clientfd) {
-            
+    auto command_elts = this->get_command_array_from_rawdata(buffer_data);
+    std::string cmd = command_elts.first;
+    std::vector<std::string> extra_params = command_elts.second;
+    if (cmd == "echo"){
+        command_processing.echo(extra_params, clientfd);
+    }
+    else if (cmd == "ping"){
+        command_processing.ping(clientfd);
+    }
+    else if (cmd == "set"){
+        for (int replica_fd: this->replicas_fd) {
+            if (send(replica_fd, buffer_data.c_str(), buffer_data.length(), 0) <= 0)
+                std::cout <<  "replica send msg failed";                
+        }
+        command_processing.set(extra_params, clientfd);
+    }
+    else if (cmd == "get"){
+        command_processing.get(extra_params, clientfd, extra_args["dir"] + "/" + extra_args["dbfilename"]);
+    }
+    else if (cmd == "config"){
+        command_processing.config(extra_params, clientfd, extra_args);
+    }
+    else if (cmd == "keys"){
+        command_processing.keys(extra_params, clientfd, extra_args["dir"] + "/" + extra_args["dbfilename"]);
+    }
+    else if (cmd == "info"){
+        command_processing.info(extra_params, clientfd, "master");
+    }
+    else if (cmd == "replconf"){
+        command_processing.replconf(extra_params, clientfd);
+    }
+    // else if (cmd == "psync"){
+    //     command_processing.psync(extra_params, clientfd, replicas_fd);
+    // }
+    
 };
 
 void SocketManagement::handle_connection(int clientfd){
