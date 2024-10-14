1726b2bb44bf2c72f895b6690d6a3da9d69722a5
unknown <franckhebo@gmail.com>
2024-10-14 23:29:12 +0100

    receive ack cmd on replica and reply

--- src/utils/command_processing.cpp
+++ src/utils/command_processing.cpp
@@ -32,7 +32,7 @@ void CommandProcessing::execute_after_delay(int delay, const std::string& key) {
 }
 
 bool CommandProcessing::send_data(std::string data, int dest_fd){
-    if (!data.empty() && GlobalDatas::isRequestFromMaster){
+    if (!data.empty() && !GlobalDatas::isRequestFromMaster){
         if (int s = send(dest_fd, data.c_str(), data.length(), 0) <= 0){
             std::cout <<  "send data failed";
             std::strerror(errno);
@@ -142,11 +142,11 @@ void CommandProcessing::info(std::vector<std::string> extras, int dest_fd, std::
 void CommandProcessing::replconf(std::vector<std::string> extras, int dest_fd){
     if (extras[0] == "listening-port" || extras[0] == "capa" && extras.size() > 1){
         std::string resp = parse_encode_simple_string(std::string("OK"));
-        send_data(resp, dest_fd);
+        send(dest_fd, resp.c_str(), resp.length(), 0)
     } else if (extras[0] == "getack" && extras[1] == "*"){
         std::vector<std::any> rep = {std::string("REPLCONF"), std::string("ACK"), std::string("0")};
         std::string resp = parse_encode_array(rep);
-        send_data(resp, dest_fd);
+        send(dest_fd, resp.c_str(), resp.length(), 0)
     }
 }
 
