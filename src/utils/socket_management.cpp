d9bfdcecbb8b2b1325b5cc283cd5be43eea2e435
unknown <franckhebo@gmail.com>
2024-10-15 00:35:12 +0100

    receive ack cmd on replica and reply

--- src/utils/socket_management.cpp
+++ src/utils/socket_management.cpp
@@ -172,16 +172,14 @@ int SocketManagement::socket_listen(int connection_backlog){
 void SocketManagement::check_incoming_clients_connections(const int& masterfd){
   struct sockaddr_in client_addr;
   int client_addr_len = sizeof(client_addr);
-//   std::vector<std::any> rep = {std::string("REPLCONF"), std::string("ACK"), std::string("0")};
-//   std::string resp = parse_encode_array(rep);
-//   CommandProcessing::send_data(resp, masterfd);
+
   if (masterfd > 0){
       GlobalDatas::isRequestFromMaster = true;
       std::thread connection([this](int master){handle_connection(master);}, masterfd);
-      connection.detach();
+      connection.join();
   }
 
-  //GlobalDatas::isRequestFromMaster = false;
+  GlobalDatas::isRequestFromMaster = false;
   std::cout << "Waiting for a client to connect...\n";
   while (1){
       int client_fd = accept(server_fd, (struct sockaddr *) &client_addr, (socklen_t *) &client_addr_len); 
