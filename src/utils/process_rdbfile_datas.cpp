e6b2cb89b34bb1298411a78e485101061658c526
unknown <franckhebo@gmail.com>
2024-10-15 13:21:17 +0100

    receive ack cmd on replica and reply

--- src/utils/process_rdbfile_datas.cpp
+++ src/utils/process_rdbfile_datas.cpp
@@ -193,7 +193,7 @@ std::pair<int, std::string> read_file_sent(char* buffer_data, int size){
     for (int i = 0; i < size; ++i) {
         myUnsignedCharArray[i] = static_cast<unsigned char>(buffer_data[i]);
     }
-    std::cout << myUnsignedCharArray[1] << " " << myUnsignedCharArray[2]  << " " << myUnsignedCharArray[3];
+    std::cout << "les éléments du tableau de char" << myUnsignedCharArray[1] << " " << myUnsignedCharArray[2]  << " " << myUnsignedCharArray[3];
     while (myUnsignedCharArray[pos] != '\r')
     {
         str_num += buffer_data[pos];
