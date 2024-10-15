cb87a6dfee67d8d333fcda60a3e7c4083facec9f
unknown <franckhebo@gmail.com>
2024-10-15 16:01:25 +0100

    receive ack cmd on replica and reply

--- src/utils/process_rdbfile_datas.cpp
+++ src/utils/process_rdbfile_datas.cpp
@@ -183,10 +183,94 @@ std::vector<unsigned char> string_to_binary(std::string str){
     return bytes;
 } 
 
+char* base64Decoder(char encoded[], int len_str)
+{
+    char* decoded_string;
+ 
+    decoded_string = (char*)malloc(sizeof(char) * 100);
+ 
+    int i, j, k = 0;
+ 
+    // stores the bitstream.
+    int num = 0;
+ 
+    // count_bits stores current
+    // number of bits in num.
+    int count_bits = 0;
+ 
+    // selects 4 characters from
+    // encoded string at a time.
+    // find the position of each encoded
+    // character in char_set and stores in num.
+    for (i = 0; i < len_str; i += 4) 
+    {
+        num = 0, count_bits = 0;
+        for (j = 0; j < 4; j++)
+        {
+             
+            // make space for 6 bits.
+            if (encoded[i + j] != '=') 
+            {
+                num = num << 6;
+                count_bits += 6;
+            }
+ 
+            /* Finding the position of each encoded 
+            character in char_set 
+            and storing in "num", use OR 
+            '|' operator to store bits.*/
+ 
+            // encoded[i + j] = 'E', 'E' - 'A' = 5
+            // 'E' has 5th position in char_set.
+            if (encoded[i + j] >= 'A' && encoded[i + j] <= 'Z')
+                num = num | (encoded[i + j] - 'A');
+ 
+            // encoded[i + j] = 'e', 'e' - 'a' = 5,
+            // 5 + 26 = 31, 'e' has 31st position in char_set.
+            else if (encoded[i + j] >= 'a' && encoded[i + j] <= 'z')
+                num = num | (encoded[i + j] - 'a' + 26);
+ 
+            // encoded[i + j] = '8', '8' - '0' = 8
+            // 8 + 52 = 60, '8' has 60th position in char_set.
+            else if (encoded[i + j] >= '0' && encoded[i + j] <= '9')
+                num = num | (encoded[i + j] - '0' + 52);
+ 
+            // '+' occurs in 62nd position in char_set.
+            else if (encoded[i + j] == '+')
+                num = num | 62;
+ 
+            // '/' occurs in 63rd position in char_set.
+            else if (encoded[i + j] == '/')
+                num = num | 63;
+ 
+            // ( str[i + j] == '=' ) remove 2 bits
+            // to delete appended bits during encoding.
+            else {
+                num = num >> 2;
+                count_bits -= 2;
+            }
+        }
+ 
+        while (count_bits != 0)
+        {
+            count_bits -= 8;
+ 
+            // 255 in binary is 11111111
+            decoded_string[k++] = (num >> count_bits) & 255;
+        }
+    }
+ 
+    // place NULL character to mark end of string.
+    decoded_string[k] = '\0';
+ 
+    return decoded_string;
+}
 std::pair<int, std::string> read_file_sent(char* buffer_data, int size){
     
     int pos = 1;
     std::string str_num;
+    if (size == 58)
+        std::cout << base64Decoder(buffer_data, 58);
     unsigned char myUnsignedCharArray[size];
 
     // Conversion de chaque élément
@@ -204,7 +288,6 @@ std::pair<int, std::string> read_file_sent(char* buffer_data, int size){
     return std::make_pair(std::stoi(str_num), file);
     
 }
-
 
 // int main(int argc, char **argv) {
 //     std::vector<std::any> str = get_keys_values_from_file("rdbfile.rdb");
