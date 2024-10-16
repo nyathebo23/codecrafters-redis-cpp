291937ca9dad80b89081f6542ae8f8bcfcf43cf0
unknown <franckhebo@gmail.com>
2024-10-16 11:14:15 +0100

    receive ack cmd on replica and reply

--- src/utils/socket_management.cpp
+++ src/utils/socket_management.cpp
@@ -243,7 +243,7 @@ void SocketManagement::retrieve_commands_from_master(int bytes_received, char* b
             process_command(cmd, array_cmd);
         }
         pos = 0;
-        std::memset(buffer, 0, size);
+        char buffer[size];
         bytes_received = recv(server_fd, &buffer, size, 0);
         std::cout << " bytes_received "  << bytes_received << " buffer " << buffer << "\n";
     }
