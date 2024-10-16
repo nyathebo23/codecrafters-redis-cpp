4ecf57d8c5697d3539939f3ab3c11445c9352cd9
unknown <franckhebo@gmail.com>
2024-10-16 23:38:38 +0100

    receive ack cmd on replica and reply

--- src/utils/command_processing.cpp
+++ src/utils/command_processing.cpp
@@ -159,6 +159,7 @@ void CommandProcessing::wait(unsigned int numreplicas, unsigned long timeout, in
                 if (it != list_replicas_fd.end()){
                     list_replicas_fd.erase(it);
                     nb_replicas_updated++;
+                    std::cout << "ertyuitdtfghjgtyhujik\n";
                 }
             }
         
