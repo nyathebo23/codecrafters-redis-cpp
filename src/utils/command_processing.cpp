36bbb8ca3bf2b86d822f58bb02a3db788b74ea81
unknown <franckhebo@gmail.com>
2024-10-17 00:02:17 +0100

    receive ack cmd on replica and reply

--- src/utils/command_processing.cpp
+++ src/utils/command_processing.cpp
@@ -85,7 +85,6 @@ void CommandProcessing::set(std::vector<std::string> extras, std::string data, i
             if (send(replica_fd_offset.first, data.c_str(), data.length(), 0) <= 0)
                 std::cout <<  "replica send msg failed";                
         }
-        std::cout << data << "\n";                
         GlobalMasterDatas::set_commands_offset(data.size(), true);
     }
     if (set_without_send(extras)){
@@ -165,7 +164,7 @@ void CommandProcessing::wait(unsigned int numreplicas, unsigned long timeout, in
         list_replicas_fd.push_back(replica_pair.first);
 
     auto limit = get_now_time_milliseconds() + timeout;
-    std::cout << GlobalMasterDatas::prec_commands_offset << "\n";
+    std::cout << "prec cmd " << GlobalMasterDatas::prec_commands_offset << "\n";
     while (get_now_time_milliseconds() < limit && nb_replicas_updated < numreplicas){
         for (auto&  replica_pair: GlobalMasterDatas::replicas_offsets)
             if (replica_pair.second == GlobalMasterDatas::prec_commands_offset){
@@ -202,7 +201,7 @@ void CommandProcessing::replconf(std::vector<std::string> extras, int dest_fd){
         try {
             int num = stoi(extras[1]);
             GlobalMasterDatas::replicas_offsets[dest_fd] = num;
-            std::cout << num << "\n";
+            std::cout << "num " << num << "\n";
         }
         catch(std::exception e){}
     }
