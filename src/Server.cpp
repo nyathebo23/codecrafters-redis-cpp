64d42a1fb9d949e5f0ef999abd0c40dab7e4e50a
unknown <franckhebo@gmail.com>
2024-10-16 13:05:48 +0100

    receive ack cmd on replica and reply

--- src/Server.cpp
+++ src/Server.cpp
@@ -55,8 +55,9 @@ int main(int argc, char **argv) {
     return 1;
   }
 
-  std::thread connection([&socket_management]() {socket_management.check_incoming_clients_connections();});
-  connection.detach();
+  // std::thread connection([&socket_management]() {socket_management.check_incoming_clients_connections();});
+  // connection.detach();
+  socket_management.check_incoming_clients_connections();
   if (args_map.count("replicaof") != 0){
       master_socket_management.send_handshake_to_master(ntohs(socket_management.get_server_addr().sin_port));
       //master_socket_management.check_incoming_master_connections();
