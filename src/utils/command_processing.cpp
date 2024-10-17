2f3cb4639b17788214369ace3820ba4561b4f85f
unknown <franckhebo@gmail.com>
2024-10-17 05:36:00 +0100

    make xadd command with error msg

--- src/utils/command_processing.cpp
+++ src/utils/command_processing.cpp
@@ -137,7 +137,7 @@ void CommandProcessing::get(std::vector<std::string> extras, int dest_fd, std::s
 std::pair<unsigned long, unsigned int> CommandProcessing::split_entry_id(std::string str){
     int ind_separator = str.find("-");
     std::cout << ind_separator << " \n";
-    unsigned long millisecond_time = std::stol(str.substr(0, ind_separator));
+    unsigned long millisecond_time = std::stoll(str.substr(0, ind_separator));
     std::cout << millisecond_time << " \n";
     unsigned int sequence_num = std::stoi(str.substr(ind_separator+1));
     std::cout << sequence_num << " " <<str.substr(ind_separator+1) <<  " " << str.size() << " "<< str.substr(0, ind_separator) << " \n";
