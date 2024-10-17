2001893ea28ca8dd62c9aa1118115258c155d0be
unknown <franckhebo@gmail.com>
2024-10-17 06:46:47 +0100

    make xadd command with error msg

--- src/utils/socket_management.cpp
+++ src/utils/socket_management.cpp
@@ -31,9 +31,13 @@ void SocketManagement::handle_connection(const int& clientfd){
             break;
         }
         std::string data(buffer);
+        std::cout << data << " \n";
         auto command_elts = CommandProcessing::get_command_array_from_rawdata(data);
         std::string cmd = command_elts.first;
         std::vector<std::string> extra_params = command_elts.second;
+        for (std::string param: extra_params)
+            std::cout << param << " ";
+        std::cout << " \n";
         if (cmd == "echo"){
             CommandProcessing::echo(extra_params, clientfd);
         }
