93badd77418d6f5c8db993e9de9cdd3ec36e8d48
unknown <franckhebo@gmail.com>
2024-10-17 14:22:07 +0100

    make xadd command with error msg and auto generated id

--- src/utils/command_processing.cpp
+++ src/utils/command_processing.cpp
@@ -181,12 +181,14 @@ void CommandProcessing::xadd(std::vector<std::string> extras, int dest_fd){
                         return;
                     }  
                 }
-            }
+            } else if (new_entry_id_str.second == "*"){
+                 id = new_entry_id_str.first + "-0";
+                 extras[1] = id;
+            } 
         }
         else if (new_entry_id_str.second == "*"){
             id = new_entry_id_str.first + "-" + (new_ms_time == 0 ? "1" : "0");
             extras[1] = id;
-            std::cout << "id " << id  << "extras1" << extras[1];
         }  
     }
     GlobalDatas::set_entry(extras);
