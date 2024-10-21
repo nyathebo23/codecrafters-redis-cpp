1f5552be8f74e6b65c922706d0986d9bbe9faec9
unknown <franckhebo@gmail.com>
2024-10-22 00:51:20 +0100

    make xread command with block params

--- src/utils/stream_commands_processing.cpp
+++ src/utils/stream_commands_processing.cpp
@@ -176,7 +176,6 @@ void StreamCommandsProcessing::xread_with_block(std::vector<std::string> extras,
     if (delay == 0){
         auto init_entry = GlobalDatas::get_entries();
         while (init_entry.size() == GlobalDatas::get_entries().size()){
-            std::this_thread::sleep_for(std::chrono::milliseconds(100));
         }
     }
     else {
