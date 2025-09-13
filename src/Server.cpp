02c21d83244bc5ed3c8d155334089d43e3ceeb30
nyathebo23 <franckhebo@gmail.com>
2025-09-13 13:43:55 +0100

    add fabric into encode and decode process, create constants to avoid repetitions

--- src/Server.cpp
+++ src/Server.cpp
@@ -1,15 +1,36 @@
 #include <iostream>
+#include <cstdlib>
 #include <string>
+#include <cstring>
 #include <unistd.h>
 #include <sys/types.h>
 #include <sys/socket.h>
 #include <arpa/inet.h>
 #include <netdb.h>
-#include <map>
+//#include <winsock2.h>
+
 
 
 int main(int argc, char **argv) {
-   if (setsockopt(server_fd, SOL_SOCKET, SO_REUSEADDR, &reuse, sizeof(reuse)) < 0) {
+  // Flush after every std::cout / std::cerr
+  std::cout << std::unitbuf;
+  std::cerr << std::unitbuf;
+
+  // You can use print statements as follows for debugging, they'll be visible when running tests.
+  std::cout << "Logs from your program will appear here!\n";
+
+  // Uncomment this block to pass the first stage
+
+  int server_fd = socket(AF_INET, SOCK_STREAM, 0);
+  if (server_fd < 0) {
+   std::cerr << "Failed to create server socket\n";
+   return 1;
+  }
+  
+  // // Since the tester restarts your program quite often, setting SO_REUSEADDR
+  // // ensures that we don't run into 'Address already in use' errors
+  int reuse = 1;
+  if (setsockopt(server_fd, SOL_SOCKET, SO_REUSEADDR, &reuse, sizeof(reuse)) < 0) {
     std::cerr << "setsockopt failed\n";
     return 1;
   }
@@ -32,10 +53,15 @@ int main(int argc, char **argv) {
   
   struct sockaddr_in client_addr;
   int client_addr_len = sizeof(client_addr);
+  
   std::cout << "Waiting for a client to connect...\n";
-  accept(server_fd, (struct sockaddr *) &client_addr, (socklen_t *) &client_addr_len);
+  
+  int client_fd = accept(server_fd, (struct sockaddr *) &client_addr, (socklen_t *) &client_addr_len);  
+  const char* message = "+PONG\r\n";
+  send(client_fd, message, strlen(message), 0);
+
   std::cout << "Client connected\n";
   close(server_fd);
+
   return 0;
 }
-
