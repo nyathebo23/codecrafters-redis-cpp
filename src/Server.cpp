8075059dac39c33392dea755b4d3c800bf3cd3c1
nyathebo23 <franckhebo@gmail.com>
2025-09-13 13:55:12 +0100

    add fabric into encode and decode process, create constants to avoid repetitions

--- src/Server.cpp
+++ src/Server.cpp
@@ -1,66 +1,70 @@
 #include <iostream>
-#include <cstdlib>
 #include <string>
-#include <cstring>
 #include <unistd.h>
 #include <sys/types.h>
 #include <sys/socket.h>
 #include <arpa/inet.h>
 #include <netdb.h>
-
-
+#include <map>
+#include "utils/socket_management.h"
+#include "globals_datas/global_datas.h"
 
 int main(int argc, char **argv) {
+    std::cout << "Waiting for a client to connect...\n";
+
+  std::map<std::string, std::string> args_map;
+  for (int i = 1; i < argc-1; i+=2){
+     args_map[std::string(argv[i]).substr(2)] = std::string(argv[i+1]);
+  }
+
+  SocketManagement socket_management(AF_INET, SOCK_STREAM, args_map);
+
+  std::map<std::string, std::string> args_map_master;
+  if (args_map.count("replicaof") != 0){
+      args_map_master["host"] = args_map["replicaof"].substr(0, args_map["replicaof"].find_first_of(" "));
+      args_map_master["port"] = args_map["replicaof"].substr(args_map["replicaof"].find_last_of(" ")+1);
+      GlobalDatas::isMaster = false;
+  }
+
+
   // Flush after every std::cout / std::cerr
   std::cout << std::unitbuf;
   std::cerr << std::unitbuf;
 
-  // You can use print statements as follows for debugging, they'll be visible when running tests.
-  std::cout << "Logs from your program will appear here!\n";
 
-  // Uncomment this block to pass the first stage
-
-  int server_fd = socket(AF_INET, SOCK_STREAM, 0);
-  if (server_fd < 0) {
+  if (socket_management.get_server_fd() < 0) {
    std::cerr << "Failed to create server socket\n";
    return 1;
   }
-  
   // // Since the tester restarts your program quite often, setting SO_REUSEADDR
   // // ensures that we don't run into 'Address already in use' errors
   int reuse = 1;
-  if (setsockopt(server_fd, SOL_SOCKET, SO_REUSEADDR, &reuse, sizeof(reuse)) < 0) {
+  if (setsockopt(socket_management.get_server_fd(), SOL_SOCKET, SO_REUSEADDR, &reuse, sizeof(reuse)) < 0) {
     std::cerr << "setsockopt failed\n";
     return 1;
   }
   
-  struct sockaddr_in server_addr;
-  server_addr.sin_family = AF_INET;
-  server_addr.sin_addr.s_addr = INADDR_ANY;
-  server_addr.sin_port = htons(6379);
-  
-  if (bind(server_fd, (struct sockaddr *) &server_addr, sizeof(server_addr)) != 0) {
-    std::cerr << "Failed to bind to port 6379\n";
+  if (socket_management.socket_bind() != 0) {
+    std::cerr << "Failed to bind to port \n";
     return 1;
   }
   
   int connection_backlog = 5;
-  if (listen(server_fd, connection_backlog) != 0) {
+  if (socket_management.socket_listen(connection_backlog) != 0) {
     std::cerr << "listen failed\n";
     return 1;
   }
-  
-  struct sockaddr_in client_addr;
-  int client_addr_len = sizeof(client_addr);
-  
-  std::cout << "Waiting for a client to connect...\n";
-  
-  int client_fd = accept(server_fd, (struct sockaddr *) &client_addr, (socklen_t *) &client_addr_len);  
-  const char* message = "+PONG\r\n";
-  send(client_fd, message, strlen(message), 0);
 
-  std::cout << "Client connected\n";
-  close(server_fd);
+  SocketManagement master_socket_management(AF_INET, SOCK_STREAM, args_map_master);
+  if (args_map.count("replicaof") != 0){
+      std::thread connection([&socket_management, &master_socket_management]() {
+        master_socket_management.handshake_and_check_incoming_master_connections(ntohs(socket_management.get_server_addr().sin_port));
+      });
+      connection.detach();
+      //master_socket_management.check_incoming_master_connections();
+  }
+
+  socket_management.check_incoming_clients_connections();
 
   return 0;
 }
