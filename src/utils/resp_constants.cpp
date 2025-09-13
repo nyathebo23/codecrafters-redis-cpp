e9ef905b0f078c313fa67c7081d4dee53f18fbbb
nyathebo23 <franckhebo@gmail.com>
2025-09-13 10:57:59 +0100

    add fabric into encode and decode process, create constants to avoid repetitions

--- src/utils/resp_constants.cpp
+++ src/utils/resp_constants.cpp
@@ -3,16 +3,17 @@
 
 const std::string okResp = parse_encode_simple_string("OK");
 const std::string pongResp = parse_encode_simple_string("PONG");
-const BulkStringEncoder replconfEnc = BulkStringEncoder("REPLCONF");
-const BulkStringEncoder ackEnc = BulkStringEncoder("ACK");
-const BulkStringEncoder getackEnc = BulkStringEncoder("GETACK");
-const BulkStringEncoder capaEnc = BulkStringEncoder("capa");
-const BulkStringEncoder portListeningEnc = BulkStringEncoder("listening-port");
-const BulkStringEncoder psync2Enc = BulkStringEncoder("psync2");
-const BulkStringEncoder psyncEnc = BulkStringEncoder("PSYNC");
-const BulkStringEncoder questionMarkEnc = BulkStringEncoder("?");
-const BulkStringEncoder minusOneEnc = BulkStringEncoder("-1");
-const BulkStringEncoder starEnc = BulkStringEncoder("*");
+const Encoder* replconfEnc = &BulkStringEncoder("REPLCONF");
+const Encoder* ackEnc = &BulkStringEncoder("ACK");
+const Encoder* getackEnc = &BulkStringEncoder("GETACK");
+const Encoder* capaEnc = &BulkStringEncoder("capa");
+const Encoder* portListeningEnc = &BulkStringEncoder("listening-port");
+const Encoder* psync2Enc = &BulkStringEncoder("psync2");
+const Encoder* psyncEnc = &BulkStringEncoder("PSYNC");
+const Encoder* questionMarkEnc = &BulkStringEncoder("?");
+const Encoder* minusOneEnc = &BulkStringEncoder("-1");
+const Encoder* starEnc = &BulkStringEncoder("*");
+
 const std::string stringTypeResp = parse_encode_simple_string("string");
 const std::string streamTypeResp = parse_encode_simple_string("stream");
 const std::string listTypeResp = parse_encode_simple_string("list");
